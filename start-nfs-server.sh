22620cb363dbaf0ec1c9b4647cffcee95cd5f78c
Brock Noland <brock@cloudera.com>
2011-12-21 18:52:06 -0600

    Add somewhat useful server startup script

--- start-nfs-server.sh
+++ start-nfs-server.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 set -e
-java -cp target/hadoop-nfs-proxy-1.0-SNAPSHOT-with-deps.jar:$(readlink -f /usr/lib/hadoop/conf) \
-    com.cloudera.hadoop.hdfs.nfs.nfs4.NFS4Server 2050 2>&1 | tee target/nfserver.out
+if [[ -f target/nfsserver.pid ]]
+then
+    pid=`<target/nfsserver.pid`
+    if [[ -n "$pid" ]] && kill -0 $pid 2>/dev/null
+    then
+        kill $pid
+        sleep 1
+        if kill -0 $pid 2>/dev/null
+        then
+            kill -9 $pid
+        fi
+    fi
+fi
+nohup java -cp target/hadoop-nfs-proxy-1.0-SNAPSHOT-with-deps.jar:$(readlink -f /usr/lib/hadoop/conf) \
+    com.cloudera.hadoop.hdfs.nfs.nfs4.NFS4Server 2050 1>target/nfsserver.out 2>target/nfsserver.err </dev/null &
+pid="$!"
+echo $pid > target/nfsserver.pid
