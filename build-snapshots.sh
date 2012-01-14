4ca98d0cba844ab383c40e280fe81df4fba5cc95
Brock Noland <brock@cloudera.com>
2012-01-14 15:28:05 -0600

    Provide snapshot jars

---
+++ build-snapshots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+git rm snapshots/* 2>/dev/null || true
+rm snapshots/* 2>/dev/null || true
+BUILDS=(
+  "hadoop-0.20  0.20.2-cdh3u2"
+  "hadoop-0.20  1.0.0"
+  "hadoop-0.23  0.23.1-SNAPSHOT"
+)
+TIME=$(date +%Y%M%d%H%M%S)
+for build in "${BUILDS[@]}"
+do
+  set -- $build
+  profile=$1
+  version=$2
+  echo $version
+  mvn clean package -P$profile -Dhadoop.version=$version 1>target/build.log 2>&1 
+  FILE=target/hadoop-nfs-proxy-*-SNAPSHOT.jar
+  VERSION=$(echo $FILE | awk -F- '{print $4}')
+  for name in "" "-with-deps"
+  do
+    OLD=target/hadoop-nfs-proxy-$VERSION-SNAPSHOT${name}.jar
+    NEW=snapshots/hadoop-nfs-proxy-$VERSION-SNAPSHOT-${version}${name}-$TIME.jar
+    mv $OLD $NEW
+  done
+done
--- start-nfs-server.sh
+++ start-nfs-server.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 set -e
-CONFIG_DIR=${1?Usage: $0 Hadoop Conf Directory e.g. /usr/lib/hadoop/conf}
-cd target
+if [[ $# -ne 2 ]]
+then
+  echo Usage: $0 HADOOP_CONF_DIR HDFS_NFS_PROXY_SNAPHOT
+  echo "e.g. $0 /usr/lib/hadoop/conf snapshots/hadoop-nfs-proxy-0.80-SNAPSHOT-with-deps-0.20.2-cdh3u2.jar"
+  exit 1
+fi
+CONFIG=$1
+JAR=$2
+if [[ ! -f $JAR ]]
+then
+  echo "Jar $JAR does not exist"
+  exit 1
+fi
+if [[ ! -e $CONFIG ]]
+then
+  echo "Config dir $CONFIG does not exist"
+  exit 1
+fi
+JAR=$(readlink -f $JAR)
+cd target 
 if [[ -f nfsserver.pid ]]
 then
-    pid=`<nfsserver.pid`
-    if [[ -n "$pid" ]] && kill -0 $pid 2>/dev/null
+  pid=`<nfsserver.pid`
+  if [[ -n "$pid" ]] && kill -0 $pid 2>/dev/null
+  then
+    kill $pid
+    sleep 1
+    if kill -0 $pid 2>/dev/null
     then
-        kill $pid
-        sleep 1
-        if kill -0 $pid 2>/dev/null
-        then
-            kill -9 $pid
-        fi
+      kill -9 $pid
     fi
+  fi
 fi
-nohup java -Xms1024m -Xmx1024m -cp hadoop-nfs-proxy-*-SNAPSHOT-with-deps.jar:$CONFIG_DIR \
-    com.cloudera.hadoop.hdfs.nfs.nfs4.NFS4Server 2049 1>nfsserver.out 2>nfsserver.err </dev/null &
+nohup java -Xms1024m -Xmx1024m -cp $JAR:$CONFIG \
+  com.cloudera.hadoop.hdfs.nfs.nfs4.NFS4Server 2049 1>nfsserver.out 2>nfsserver.err </dev/null &
 pid="$!"
 echo $pid > nfsserver.pid
