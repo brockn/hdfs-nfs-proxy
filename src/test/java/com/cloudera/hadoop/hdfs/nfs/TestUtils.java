9ffbff50b3206cc14181a5d87c5935361b5ebcba
hanasaki <hanasaki@usa>
2012-01-18 21:00:49 -0600

    pom.xml : add profile information for hadoop 1.0.0 and set properties make tmp files for testing on demand instead of using system /etc files - security

--- src/test/java/com/cloudera/hadoop/hdfs/nfs/TestUtils.java
+++ src/test/java/com/cloudera/hadoop/hdfs/nfs/TestUtils.java
@@ -1,21 +1,20 @@
 /**
  * Copyright 2011 The Apache Software Foundation
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.cloudera.hadoop.hdfs.nfs;
 
@@ -38,72 +37,157 @@ import com.cloudera.hadoop.hdfs.nfs.rpc.RPCBuffer;
 import com.cloudera.hadoop.hdfs.nfs.rpc.RPCPacket;
 import com.cloudera.hadoop.hdfs.nfs.security.AuthenticatedCredentials;
 import com.cloudera.hadoop.hdfs.nfs.security.CredentialsSystem;
+import java.io.*;
+import java.util.Date;
+import java.util.Random;
 
 public class TestUtils {
-  protected static final Logger LOGGER = LoggerFactory.getLogger(TestUtils.class);
 
-  public static Configuration setupConf() throws IOException {
-    return setupConf(new Configuration());
-  }
-  public static Configuration setupConf(Configuration conf) throws IOException {
-    conf.set(NFS_FILEHANDLE_STORE_FILE, "/tmp/" + UUID.randomUUID().toString());
-    return conf;
-  }
+    protected static final Logger LOGGER = LoggerFactory.getLogger(TestUtils.class);
+    final static public String tmpDirPath;
+    final static public String tmpDirPathForTest;
 
-  public static AuthenticatedCredentials newCredentials() {
-    CredentialsSystem creds = new CredentialsSystem();
-    creds.setUID(0);
-    creds.setGID(0);
-    creds.setAuxGIDs(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    return creds;
-  }
-  public static void copy(MessageBase base, MessageBase copy) {
-    RPCBuffer buffer = new RPCBuffer();
-    base.write(buffer);
-    buffer.flip();
-    if(base instanceof RPCPacket) {
-      buffer.skip(4); // size
+    static {
+        tmpDirPath = System.getProperty("java.io.tmpdir");
+        tmpDirPathForTest = tmpDirPath + File.separator
+                + "TestDir" + (new Date()).getTime();
     }
-    copy.read(buffer);
-  }
 
-  public static void deepEquals(MessageBase base, MessageBase copy) {
-    AtomicInteger count = new AtomicInteger(0);
-    deepEquals(base, copy, count);
-    if(count.get() <= 0) {
-      LOGGER.error("Did not test any methods for " + base.getClass().getName());
+    public static Configuration setupConf() throws IOException {
+        return setupConf(new Configuration());
     }
-  }
-  protected static void deepEquals(MessageBase base, MessageBase copy, AtomicInteger count) {
-    assertEquals(base.getClass(), copy.getClass());
-    String prefix = null;
-    try {
-      Class<?> clazz = base.getClass(); 
-      for (Method method : clazz.getMethods()) {
-        prefix = clazz.getName() + ":" + method.getName();
-        int mod = method.getModifiers();
-        if(method.getName().startsWith("get") && method.getParameterTypes().length == 0 &&
-            !(Modifier.isStatic(mod) || Modifier.isAbstract(mod) || Modifier.isNative(mod))) {
-          Object baseResult = method.invoke(base, (Object[])null);
-          Object copyResult = method.invoke(base, (Object[])null);
-          if (baseResult instanceof MessageBase) {
-            deepEquals((MessageBase)baseResult, (MessageBase)copyResult, count);
-          } else if (baseResult instanceof List) {
-            List<?> baseList = (List<?>)baseResult;
-            List<?> copyList = (List<?>)copyResult;
-            assertEquals(baseList.size(), copyList.size());
-            for (int i = 0; i < baseList.size(); i++) {
-              count.addAndGet(1);
-              assertEquals(baseList.get(i), copyList.get(i));
+
+    public static String buildTempDirDataFiles(
+            final int numberOfFiles,
+            final String path) throws IOException {
+        String retVal = tmpDirPathForTest;
+        File dir = new File(retVal);
+        boolean mkdirsSuccess = dir.mkdirs();
+        if (!mkdirsSuccess) {
+            File test = new File(retVal);
+            if (!test.exists()) {
+                throw new IOException("failed to make dir -> " + dir.getAbsolutePath());
             }
-          } else {
-            count.addAndGet(1);
-            assertEquals(prefix, baseResult, copyResult);            
-          }
         }
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(prefix, e);
+        for (int i = 0; i < numberOfFiles; i++) {
+            createTempFile(path, true, 2048, TestUtils.class.getClass().getName(), Integer.valueOf(i).toString());
+        }
+        return (retVal);
     }
-  }
+
+    public static Configuration setupConf(Configuration conf) throws IOException {
+        conf.set(NFS_FILEHANDLE_STORE_FILE, "/tmp/" + UUID.randomUUID().toString());
+        return conf;
+    }
+
+    public static AuthenticatedCredentials newCredentials() {
+        CredentialsSystem creds = new CredentialsSystem();
+        creds.setUID(0);
+        creds.setGID(0);
+        creds.setAuxGIDs(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        return creds;
+    }
+
+    public static void copy(MessageBase base, MessageBase copy) {
+        RPCBuffer buffer = new RPCBuffer();
+        base.write(buffer);
+        buffer.flip();
+        if (base instanceof RPCPacket) {
+            buffer.skip(4); // size
+        }
+        copy.read(buffer);
+    }
+
+    public static void deepEquals(MessageBase base, MessageBase copy) {
+        AtomicInteger count = new AtomicInteger(0);
+        deepEquals(base, copy, count);
+        if (count.get() <= 0) {
+            LOGGER.error("Did not test any methods for " + base.getClass().getName());
+        }
+    }
+
+    /**
+     * create a temp file with a random set of ascii characters
+     *
+     * @author hanasaki
+     * @param size
+     * @param fnRequested
+     * @return
+     * @throws IOException
+     */
+    public static File createTempFile(
+            final String path,
+            final boolean deleteOnExit,
+            final int size,
+            final String fnRequested,
+            final String fnSuffix)
+            throws IOException {
+        File retVal;
+        //
+        String fn = (fnRequested == null) ? "testFile" : fnRequested;
+        fn += (fnSuffix == null) ? "" : "-" + fnSuffix;
+
+        if (path == null) {
+            retVal = File.createTempFile(fn, ".tmp");
+        } else {
+            File dir = new File(path);
+            retVal = File.createTempFile(fn, ".tmp", dir);
+        }
+        if (deleteOnExit) {
+            retVal.deleteOnExit();
+        }
+
+        OutputStream o = new BufferedOutputStream(new FileOutputStream(retVal));
+
+        //
+        Random random = new Random();
+        final short start = ' ';
+        final short end = '}';
+        final short cnt = end - start;
+        //
+        for (int i = 0; i < size; i++) {
+            short r = (short) random.nextInt(cnt);
+            byte value = (byte) (start + r);
+            o.write(value);
+        }
+        o.flush();
+        o.close();
+
+        LOGGER.debug("created " + size + " byte tmp file => " + retVal.getCanonicalPath());
+
+        return (retVal);
+    }
+
+    protected static void deepEquals(MessageBase base, MessageBase copy, AtomicInteger count) {
+        assertEquals(base.getClass(), copy.getClass());
+        String prefix = null;
+        try {
+            Class<?> clazz = base.getClass();
+            for (Method method : clazz.getMethods()) {
+                prefix = clazz.getName() + ":" + method.getName();
+                int mod = method.getModifiers();
+                if (method.getName().startsWith("get") && method.getParameterTypes().length == 0
+                        && !(Modifier.isStatic(mod) || Modifier.isAbstract(mod) || Modifier.isNative(mod))) {
+                    Object baseResult = method.invoke(base, (Object[]) null);
+                    Object copyResult = method.invoke(base, (Object[]) null);
+                    if (baseResult instanceof MessageBase) {
+                        deepEquals((MessageBase) baseResult, (MessageBase) copyResult, count);
+                    } else if (baseResult instanceof List) {
+                        List<?> baseList = (List<?>) baseResult;
+                        List<?> copyList = (List<?>) copyResult;
+                        assertEquals(baseList.size(), copyList.size());
+                        for (int i = 0; i < baseList.size(); i++) {
+                            count.addAndGet(1);
+                            assertEquals(baseList.get(i), copyList.get(i));
+                        }
+                    } else {
+                        count.addAndGet(1);
+                        assertEquals(prefix, baseResult, copyResult);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(prefix, e);
+        }
+    }
 }
--- src/test/java/com/cloudera/hadoop/hdfs/nfs/nfs4/TestWithClient.java
+++ src/test/java/com/cloudera/hadoop/hdfs/nfs/nfs4/TestWithClient.java
@@ -1,21 +1,20 @@
 /**
  * Copyright 2011 The Apache Software Foundation
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.cloudera.hadoop.hdfs.nfs.nfs4;
 
@@ -36,193 +35,220 @@ import org.apache.hadoop.util.Shell;
 import org.junit.Test;
 
 import com.cloudera.hadoop.hdfs.nfs.NFSUtils;
+import com.cloudera.hadoop.hdfs.nfs.TestUtils;
 import com.google.common.collect.ImmutableList;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 
 public class TestWithClient {
-  
-  
-  @Test
-  public void testNOENTLocal() throws IOException, InterruptedException, NFS4Exception {
-    testNOENT(new LocalClient());
-  }
 
-  @Test
-  public void testNOENTNetwork() throws IOException, InterruptedException, NFS4Exception {
-    testNOENT(new NetworkClient());
-  }
+    protected static final Logger LOGGER = Logger.getLogger(TestWithClient.class);
+    private File testFile1;
+    private File testFile2;
+    private final static String className = TestWithClient.class.getName();
 
-  public void testNOENT(BaseClient client) throws IOException, InterruptedException, NFS4Exception {
-    try {
-      client.listPath(new Path("/" + UUID.randomUUID().toString()));      
-      fail("Expected no such file or directory");
-    } catch (NFS4Exception e) {
-      assertEquals(NFS4ERR_NOENT, e.getError());
-    } finally {
-      client.shutdown();
+    @Before
+    public void setup() throws IOException {
+        TestUtils.buildTempDirDataFiles(6, TestUtils.tmpDirPathForTest);
+        testFile1 = TestUtils.createTempFile(null, true, 1024, className, null);
+        testFile2 = TestUtils.createTempFile(null, true, 1024, className, null);
     }
-  }
-  
-  @Test
-  public void testReadDirLocal() throws IOException, InterruptedException, NFS4Exception {
-    doReadDir(new LocalClient());
-  }
 
-  @Test
-  public void testReadDirNetwork() throws IOException, InterruptedException, NFS4Exception {
-    doReadDir(new NetworkClient());
-  }
-
-  public void doReadDir(BaseClient client) throws IOException, InterruptedException, NFS4Exception {
-    /*
-     * traverse through a directory that does not change often
-     * and ensure it checks out the same as through the native api
-     */
-    Path rootPath = new Path("/");
-    Path etcPath = new Path("etc/init.d");
-    compareFileStatusFile(client.getFileStatus(rootPath));
-    ImmutableList<Path> paths = client.listPath(new Path(rootPath, etcPath));
-    File etcFile = new File(rootPath.toString(), etcPath.toString());
-    assertEquals(etcFile.list().length, paths.size());
-    for (Path path : paths) {
-      compareFileStatusFile(client.getFileStatus(path));
+    @After
+    public void teardown() {
+        testFile1.delete();
+        testFile2.delete();
     }
-    
-    client.shutdown();
-  }
-  
-  @Test
-  public void testSmallReadSizeLocal() throws Exception {
-    doSmallReadSize(new LocalClient());
-  }
-  
-  @Test
-  public void testSmallReadSizeNetwork() throws Exception {
-    doSmallReadSize(new NetworkClient());
-  }
-  
-  public void doSmallReadSize(BaseClient client) throws Exception {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(client.forRead(new Path("/etc/passwd"), 5)));
-    String line;
-    
-    boolean foundRoot = false;
-    while((line = reader.readLine()) != null) {
-      if(line.startsWith("root")) {
-        foundRoot = true;
-        // don't break on purpose (more requests)
-      }
-    }  
-    reader.close();
-    client.shutdown();
-    assertTrue(foundRoot);
-  }
-  
-  @Test
-  public void testLargeReadSizeLocal() throws Exception {
-    doLargeReadSize(new LocalClient());
-  }
-  
-  @Test
-  public void testLargeReadSizeNetwork() throws Exception {
-    doLargeReadSize(new NetworkClient());
-  }
-  
-  public void doLargeReadSize(BaseClient client) throws Exception {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(client.forRead(new Path("/etc/passwd"), 1024 * 1024)));
-    String line;
-    
-    boolean foundRoot = false;
-    while((line = reader.readLine()) != null) {
-      if(line.startsWith("root")) {
-        foundRoot = true;
-        // don't break on purpose (more requests)
-      }
-    }  
-    reader.close();
-    client.shutdown();
-    assertTrue(foundRoot);
-  }
 
-  @Test
-  public void testNormalReadSizeLocal() throws Exception {
-    doNormalReadSize(new LocalClient());
-  }
-  
-  @Test
-  public void testNormalReadSizeNetwork() throws Exception {
-    doNormalReadSize(new NetworkClient());
-  }
-  public void doNormalReadSize(BaseClient client) throws Exception {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(client.forRead(new Path("/etc/passwd"), NFS4_MAX_RWSIZE)));
-    String line;
-    
-    boolean foundRoot = false;
-    while((line = reader.readLine()) != null) {
-      if(line.startsWith("root")) {
-        foundRoot = true;
-        // don't break on purpose (more requests)
-      }
-    }  
-    reader.close();
-    client.shutdown();
-    assertTrue(foundRoot);
-  }
-  
-  @Test
-  public void testWriteLocal() throws Exception {
-    doWrite(new LocalClient());
-  }
-  
-  @Test
-  public void testWriteNetwork() throws Exception {
-    doWrite(new NetworkClient());
-  }
-  
-  public void doWrite(BaseClient client) throws Exception {
-    File file = new File("/tmp", UUID.randomUUID().toString());
-    Path path = new Path(file.getAbsolutePath());
-    try {
-      OutputStream out = client.forWrite(path);
-      for (int i = 0; i < 1000; i++) {
-        out.write((byte)'A');
-      }
-      out.close();
-      InputStream in = client.forRead(path, NFS4_MAX_RWSIZE);
-      int b = in.read();
-      while(b >= 0) {
-        assertTrue("Byte = '" + String.valueOf((char)b) + "'", (char)b == 'A');
-        b = in.read();
-      }
-      in.close();
-    } finally {
-      file.delete();
-      client.shutdown();
+    @Test
+    public void testNOENTLocal() throws IOException, InterruptedException, NFS4Exception {
+        testNOENT(new LocalClient());
     }
-  }
-  
-  protected String getOwner(File file) throws IOException {
-    String[] cmd =  new String[]{"stat","-c","%U",file.getAbsolutePath()};
-    return Shell.execCommand(cmd).trim();
-  }
-  protected String getOwnerGroup(File file) throws IOException {
-    String[] cmd =  new String[]{"/usr/bin/stat","-c","%G", file.getAbsolutePath()};
-    return Shell.execCommand(cmd).trim();
-  }
-  protected void doCompareFileStatusFile(FileStatus fileStatus) throws IOException {
-    File file = new File(fileStatus.path.toString());
-    String domain = NFSUtils.getDomain(new Configuration(), LOCALHOST);
-    assertEquals(getOwner(file) + "@" + domain, fileStatus.getOwner());
-    assertEquals(getOwnerGroup(file) + "@" + domain, fileStatus.getOwnerGroup());
-    assertEquals(file.length(), fileStatus.getSize());
-    assertEquals(file.lastModified(), fileStatus.getMTime());      
-    assertEquals(file.isDirectory(), fileStatus.isDir());
-  }
-  protected void compareFileStatusFile(FileStatus fileStatus) throws IOException, InterruptedException {
-    try {
-      doCompareFileStatusFile(fileStatus);
-    } catch(AssertionError error) {
-      Thread.sleep(100L);
-      doCompareFileStatusFile(fileStatus);
-    }
-  }
 
+    @Test
+    public void testNOENTNetwork() throws IOException, InterruptedException, NFS4Exception {
+        testNOENT(new NetworkClient());
+    }
+
+    public void testNOENT(BaseClient client) throws IOException, InterruptedException, NFS4Exception {
+        try {
+            client.listPath(new Path("/" + UUID.randomUUID().toString()));
+            fail("Expected no such file or directory");
+        } catch (NFS4Exception e) {
+            assertEquals(NFS4ERR_NOENT, e.getError());
+        } finally {
+            client.shutdown();
+        }
+    }
+
+    @Test
+    public void testReadDirLocal() throws IOException, InterruptedException, NFS4Exception {
+        doReadDir(new LocalClient());
+    }
+
+    @Test
+    public void testReadDirNetwork() throws IOException, InterruptedException, NFS4Exception {
+        doReadDir(new NetworkClient());
+    }
+
+    public void doReadDir(BaseClient client) throws IOException, InterruptedException, NFS4Exception {
+        /*
+         * traverse through a directory that does not change often and ensure it
+         * checks out the same as through the native api
+         */
+        Path rootPath = new Path("/");
+        Path etcPath = new Path(TestUtils.tmpDirPathForTest);
+        compareFileStatusFile(client.getFileStatus(rootPath));
+        ImmutableList<Path> paths = client.listPath(new Path(rootPath, etcPath));
+        File etcFile = new File(rootPath.toString(), etcPath.toString());
+        assertEquals(etcFile.list().length, paths.size());
+        for (Path path : paths) {
+            LOGGER.debug("checking file => " + path.getName());
+            compareFileStatusFile(client.getFileStatus(path));
+        }
+
+        client.shutdown();
+    }
+
+    @Test
+    public void testSmallReadSizeLocal() throws Exception {
+        doSmallReadSize(new LocalClient());
+    }
+
+    @Test
+    public void testSmallReadSizeNetwork() throws Exception {
+        doSmallReadSize(new NetworkClient());
+    }
+
+    public void doSmallReadSize(BaseClient client) throws Exception {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                client.forRead(new Path(testFile1.getAbsolutePath()), 5)));
+        String line;
+
+        boolean foundRoot = false;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("root")) {
+                foundRoot = true;
+                // don't break on purpose (more requests)
+            }
+        }
+        reader.close();
+        client.shutdown();
+        assertTrue(foundRoot);
+    }
+
+    @Test
+    public void testLargeReadSizeLocal() throws Exception {
+        doLargeReadSize(new LocalClient());
+    }
+
+    @Test
+    public void testLargeReadSizeNetwork() throws Exception {
+        doLargeReadSize(new NetworkClient());
+    }
+
+    public void doLargeReadSize(BaseClient client) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(client.forRead(new Path(testFile1.getCanonicalPath()), 1024 * 1024)));
+        String line;
+
+        boolean foundRoot = false;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("root")) {
+                foundRoot = true;
+                // don't break on purpose (more requests)
+            }
+        }
+        reader.close();
+        client.shutdown();
+        assertTrue(foundRoot);
+    }
+
+    @Test
+    public void testNormalReadSizeLocal() throws Exception {
+        doNormalReadSize(new LocalClient());
+    }
+
+    @Test
+    public void testNormalReadSizeNetwork() throws Exception {
+        doNormalReadSize(new NetworkClient());
+    }
+
+    public void doNormalReadSize(BaseClient client) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(client.forRead(new Path(testFile1.getCanonicalPath()), NFS4_MAX_RWSIZE)));
+        String line;
+
+        boolean foundRoot = false;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("root")) {
+                foundRoot = true;
+                // don't break on purpose (more requests)
+            }
+        }
+        reader.close();
+        client.shutdown();
+        assertTrue(foundRoot);
+    }
+
+    @Test
+    public void testWriteLocal() throws Exception {
+        doWrite(new LocalClient());
+    }
+
+    @Test
+    public void testWriteNetwork() throws Exception {
+        doWrite(new NetworkClient());
+    }
+
+    public void doWrite(BaseClient client) throws Exception {
+        File file = new File("/tmp", UUID.randomUUID().toString());
+        Path path = new Path(file.getAbsolutePath());
+        try {
+            OutputStream out = client.forWrite(path);
+            for (int i = 0; i < 1000; i++) {
+                out.write((byte) 'A');
+            }
+            out.close();
+            InputStream in = client.forRead(path, NFS4_MAX_RWSIZE);
+            int b = in.read();
+            while (b >= 0) {
+                assertTrue("Byte = '" + String.valueOf((char) b) + "'", (char) b == 'A');
+                b = in.read();
+            }
+            in.close();
+        } finally {
+            file.delete();
+            client.shutdown();
+        }
+    }
+
+    protected String getOwner(File file) throws IOException {
+        String[] cmd = new String[]{"stat", "-c", "%U", file.getCanonicalPath()};
+        return Shell.execCommand(cmd).trim();
+    }
+
+    protected String getOwnerGroup(File file) throws IOException {
+        String[] cmd = new String[]{"/usr/bin/stat", "-c", "%G", file.getAbsolutePath()};
+        return Shell.execCommand(cmd).trim();
+    }
+
+    protected void doCompareFileStatusFile(FileStatus fileStatus) throws IOException {
+        File file = new File(fileStatus.path.toString());
+        String domain = NFSUtils.getDomain(new Configuration(), LOCALHOST);
+        assertEquals(getOwner(file) + "@" + domain, fileStatus.getOwner());
+        assertEquals(getOwnerGroup(file) + "@" + domain, fileStatus.getOwnerGroup());
+        assertEquals(file.length(), fileStatus.getSize());
+        assertEquals(file.lastModified(), fileStatus.getMTime());
+        assertEquals(file.isDirectory(), fileStatus.isDir());
+    }
+
+    protected void compareFileStatusFile(FileStatus fileStatus) throws IOException, InterruptedException {
+        try {
+            doCompareFileStatusFile(fileStatus);
+        } catch (AssertionError error) {
+            Thread.sleep(100L);
+            doCompareFileStatusFile(fileStatus);
+        }
+    }
 }
