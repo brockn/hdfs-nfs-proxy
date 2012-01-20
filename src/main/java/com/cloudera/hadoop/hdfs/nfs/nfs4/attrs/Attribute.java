203e5706a0d0ea97bebc15ca99b5ffe0136eafc6
Brock Noland <brock@cloudera.com>
2012-01-19 21:31:43 -0600

    Fix formatting of Attribute class

--- src/main/java/com/cloudera/hadoop/hdfs/nfs/nfs4/attrs/Attribute.java
+++ src/main/java/com/cloudera/hadoop/hdfs/nfs/nfs4/attrs/Attribute.java
@@ -58,15 +58,52 @@ public abstract class Attribute implements MessageBase, Identifiable {
     }
     
     //TODO Brock?? want to breakup this long line into a couple lines of statements that read easier?
+    // Yes this had always been multiple lines for me, not sure how it got into one long line? Did
+    // you run an atuomatic reformater?
     static ImmutableMap<Integer, AttributeHolder> attributes =
-            ImmutableMap.<Integer, AttributeHolder>builder().put(
-            NFS4_FATTR4_ACL_SUPPORT, 
-            new AttributeHolder(ACLSupport.class,
-            new ACLSupportHandler())).put(NFS4_FATTR4_CHANGE, 
-            new AttributeHolder(ChangeID.class, new ChangeIDHandler())).put(NFS4_FATTR4_CASE_INSENSITIVE,
-            new AttributeHolder(CaseInsensitive.class,
-            new CaseInsensitiveHandler())).put(NFS4_FATTR4_CASE_PRESERVING,
-            new AttributeHolder(CasePreserving.class, new CasePreservingHandler())).put(NFS4_FATTR4_CHOWN_RESTRICTED, new AttributeHolder(ChownRestricted.class, new ChownRestrictedHandler())).put(NFS4_FATTR4_TIME_CREATE, new AttributeHolder(CreateTime.class, new CreateTimeHandler())).put(NFS4_FATTR4_FILEID, new AttributeHolder(FileID.class, new FileIDHandler())).put(NFS4_FATTR4_FILEHANDLE, new AttributeHolder(FileHandle.class, new FileHandleHandler())).put(NFS4_FATTR4_FILES_AVAIL, new AttributeHolder(FilesAvailable.class, new FilesAvailableHandler())).put(NFS4_FATTR4_FILES_FREE, new AttributeHolder(FilesFree.class, new FilesFreeHandler())).put(NFS4_FATTR4_FILES_TOTAL, new AttributeHolder(FilesTotal.class, new FilesTotalHandler())).put(NFS4_FATTR4_FH_EXPIRE_TYPE, new AttributeHolder(FHExpireType.class, new FHExpireTypeHandler())).put(NFS4_FATTR4_FSID, new AttributeHolder(FileSystemID.class, new FileSystemIDHandler())).put(NFS4_FATTR4_HOMOGENEOUS, new AttributeHolder(Homogeneous.class, new HomogeneousHandler())).put(NFS4_FATTR4_LINK_SUPPORT, new AttributeHolder(LinkSupport.class, new LinkSupportHandler())).put(NFS4_FATTR4_LEASE_TIME, new AttributeHolder(LeaseTime.class, new LeaseTimeHandler())).put(NFS4_FATTR4_UNIQUE_HANDLES, new AttributeHolder(UniqueHandlesSupport.class, new UniqueHandlesSupportHandler())).put(NFS4_FATTR4_MAXFILESIZE, new AttributeHolder(MaxFile.class, new MaxFileHandler())).put(NFS4_FATTR4_MAXLINK, new AttributeHolder(MaxLink.class, new MaxLinkHandler())).put(NFS4_FATTR4_MAXNAME, new AttributeHolder(MaxName.class, new MaxNameHandler())).put(NFS4_FATTR4_MAXREAD, new AttributeHolder(MaxRead.class, new MaxReadHandler())).put(NFS4_FATTR4_MAXWRITE, new AttributeHolder(MaxWrite.class, new MaxWriteHandler())).put(NFS4_FATTR4_MODE, new AttributeHolder(Mode.class, new ModeHandler())).put(NFS4_FATTR4_NAMED_ATTR, new AttributeHolder(NamedAttrSupport.class, new NamedAttrSupportHandler())).put(NFS4_FATTR4_NUMLINKS, new AttributeHolder(NumLink.class, new NumLinkHandler())).put(NFS4_FATTR4_NO_TRUNC, new AttributeHolder(NoTruncate.class, new NoTruncateHandler())).put(NFS4_FATTR4_OWNER, new AttributeHolder(Owner.class, new OwnerHandler())).put(NFS4_FATTR4_OWNER_GROUP, new AttributeHolder(OwnerGroup.class, new OwnerGroupHandler())).put(NFS4_FATTR4_RAWDEV, new AttributeHolder(RawDevice.class, new RawDeviceHandler())).put(NFS4_FATTR4_SIZE, new AttributeHolder(Size.class, new SizeHandler())).put(NFS4_FATTR4_SUPPORTED_ATTRS, new AttributeHolder(SupportedAttributes.class, new SupportedAttributesHandler())).put(NFS4_FATTR4_SPACE_AVAIL, new AttributeHolder(SpaceAvailable.class, new SpaceAvailableHandler())).put(NFS4_FATTR4_SPACE_FREE, new AttributeHolder(SpaceFree.class, new SpaceFreeHandler())).put(NFS4_FATTR4_SPACE_USED, new AttributeHolder(SpaceUsed.class, new SpaceUsedHandler())).put(NFS4_FATTR4_SPACE_TOTAL, new AttributeHolder(SpaceTotal.class, new SpaceTotalHandler())).put(NFS4_FATTR4_SYMLINK_SUPPORT, new AttributeHolder(SymLinkSupport.class, new SymLinkSupportHandler())).put(NFS4_FATTR4_TIME_ACCESS, new AttributeHolder(AccessTime.class, new AccessTimeHandler())).put(NFS4_FATTR4_TIME_ACCESS_SET, new AttributeHolder(SetAccessTime.class, new SetAccessTimeHandler())).put(NFS4_FATTR4_TIME_METADATA, new AttributeHolder(MetadataTime.class, new MetadataTimeHandler())).put(NFS4_FATTR4_TIME_MODIFY, new AttributeHolder(ModifyTime.class, new ModifyTimeHandler())).put(NFS4_FATTR4_TIME_MODIFY_SET, new AttributeHolder(SetModifyTime.class, new SetModifyTimeHandler())).put(NFS4_FATTR4_TYPE, new AttributeHolder(Type.class, new TypeHandler())).build();
+            ImmutableMap.<Integer, AttributeHolder>builder()
+            .put(NFS4_FATTR4_ACL_SUPPORT, new AttributeHolder(ACLSupport.class, new ACLSupportHandler()))
+            .put(NFS4_FATTR4_CHANGE, new AttributeHolder(ChangeID.class, new ChangeIDHandler()))
+            .put(NFS4_FATTR4_CASE_INSENSITIVE, new AttributeHolder(CaseInsensitive.class, new CaseInsensitiveHandler()))
+            .put(NFS4_FATTR4_CASE_PRESERVING, new AttributeHolder(CasePreserving.class, new CasePreservingHandler()))
+            .put(NFS4_FATTR4_CHOWN_RESTRICTED, new AttributeHolder(ChownRestricted.class, new ChownRestrictedHandler()))
+            .put(NFS4_FATTR4_TIME_CREATE, new AttributeHolder(CreateTime.class, new CreateTimeHandler()))
+            .put(NFS4_FATTR4_FILEID, new AttributeHolder(FileID.class, new FileIDHandler()))
+            .put(NFS4_FATTR4_FILEHANDLE, new AttributeHolder(FileHandle.class, new FileHandleHandler()))
+            .put(NFS4_FATTR4_FILES_AVAIL, new AttributeHolder(FilesAvailable.class, new FilesAvailableHandler()))
+            .put(NFS4_FATTR4_FILES_FREE, new AttributeHolder(FilesFree.class, new FilesFreeHandler()))
+            .put(NFS4_FATTR4_FILES_TOTAL, new AttributeHolder(FilesTotal.class, new FilesTotalHandler()))
+            .put(NFS4_FATTR4_FH_EXPIRE_TYPE, new AttributeHolder(FHExpireType.class, new FHExpireTypeHandler()))
+            .put(NFS4_FATTR4_FSID, new AttributeHolder(FileSystemID.class, new FileSystemIDHandler()))
+            .put(NFS4_FATTR4_HOMOGENEOUS, new AttributeHolder(Homogeneous.class, new HomogeneousHandler()))
+            .put(NFS4_FATTR4_LINK_SUPPORT, new AttributeHolder(LinkSupport.class, new LinkSupportHandler()))
+            .put(NFS4_FATTR4_LEASE_TIME, new AttributeHolder(LeaseTime.class, new LeaseTimeHandler()))
+            .put(NFS4_FATTR4_UNIQUE_HANDLES, new AttributeHolder(UniqueHandlesSupport.class, new UniqueHandlesSupportHandler()))
+            .put(NFS4_FATTR4_MAXFILESIZE, new AttributeHolder(MaxFile.class, new MaxFileHandler()))
+            .put(NFS4_FATTR4_MAXLINK, new AttributeHolder(MaxLink.class, new MaxLinkHandler()))
+            .put(NFS4_FATTR4_MAXNAME, new AttributeHolder(MaxName.class, new MaxNameHandler()))
+            .put(NFS4_FATTR4_MAXREAD, new AttributeHolder(MaxRead.class, new MaxReadHandler()))
+            .put(NFS4_FATTR4_MAXWRITE, new AttributeHolder(MaxWrite.class, new MaxWriteHandler()))
+            .put(NFS4_FATTR4_MODE, new AttributeHolder(Mode.class, new ModeHandler()))
+            .put(NFS4_FATTR4_NAMED_ATTR, new AttributeHolder(NamedAttrSupport.class, new NamedAttrSupportHandler()))
+            .put(NFS4_FATTR4_NUMLINKS, new AttributeHolder(NumLink.class, new NumLinkHandler()))
+            .put(NFS4_FATTR4_NO_TRUNC, new AttributeHolder(NoTruncate.class, new NoTruncateHandler()))
+            .put(NFS4_FATTR4_OWNER, new AttributeHolder(Owner.class, new OwnerHandler()))
+            .put(NFS4_FATTR4_OWNER_GROUP, new AttributeHolder(OwnerGroup.class, new OwnerGroupHandler()))
+            .put(NFS4_FATTR4_RAWDEV, new AttributeHolder(RawDevice.class, new RawDeviceHandler()))
+            .put(NFS4_FATTR4_SIZE, new AttributeHolder(Size.class, new SizeHandler()))
+            .put(NFS4_FATTR4_SUPPORTED_ATTRS, new AttributeHolder(SupportedAttributes.class, new SupportedAttributesHandler()))
+            .put(NFS4_FATTR4_SPACE_AVAIL, new AttributeHolder(SpaceAvailable.class, new SpaceAvailableHandler()))
+            .put(NFS4_FATTR4_SPACE_FREE, new AttributeHolder(SpaceFree.class, new SpaceFreeHandler()))
+            .put(NFS4_FATTR4_SPACE_USED, new AttributeHolder(SpaceUsed.class, new SpaceUsedHandler()))
+            .put(NFS4_FATTR4_SPACE_TOTAL, new AttributeHolder(SpaceTotal.class, new SpaceTotalHandler()))
+            .put(NFS4_FATTR4_SYMLINK_SUPPORT, new AttributeHolder(SymLinkSupport.class, new SymLinkSupportHandler()))
+            .put(NFS4_FATTR4_TIME_ACCESS, new AttributeHolder(AccessTime.class, new AccessTimeHandler()))
+            .put(NFS4_FATTR4_TIME_ACCESS_SET, new AttributeHolder(SetAccessTime.class, new SetAccessTimeHandler()))
+            .put(NFS4_FATTR4_TIME_METADATA, new AttributeHolder(MetadataTime.class, new MetadataTimeHandler()))
+            .put(NFS4_FATTR4_TIME_MODIFY, new AttributeHolder(ModifyTime.class, new ModifyTimeHandler()))
+            .put(NFS4_FATTR4_TIME_MODIFY_SET, new AttributeHolder(SetModifyTime.class, new SetModifyTimeHandler()))
+            .put(NFS4_FATTR4_TYPE, new AttributeHolder(Type.class, new TypeHandler())).build();
 
     public static Bitmap getSupported() {
         Bitmap attrs = new Bitmap();
