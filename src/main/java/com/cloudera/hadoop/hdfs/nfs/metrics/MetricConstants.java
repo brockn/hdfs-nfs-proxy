4c0724ba0ca9f959d7815c8177ad1dccaba6342d
Brock Noland <brock@cloudera.com>
2012-11-10 11:40:30 -0600

    Copied bad Copyright notice. Should be ASL2 with Cloudera Copyright

--- src/main/java/com/cloudera/hadoop/hdfs/nfs/metrics/MetricConstants.java
+++ src/main/java/com/cloudera/hadoop/hdfs/nfs/metrics/MetricConstants.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 The Apache Software Foundation
+ * Copyright 2012 Cloudera Inc.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with this
